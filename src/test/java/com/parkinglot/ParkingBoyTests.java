27a05aaf117eabc1b52f022afeafe4f9d6f80df4
KiraYous <404465394@qq.com>
2022-07-22 16:24:35 +0800

    test: pass get twice car with twice ticket by parkingboy

--- src/test/java/com/parkinglot/ParkingBoyTests.java
+++ src/test/java/com/parkinglot/ParkingBoyTests.java
@@ -21,20 +21,23 @@ public class ParkingBoyTests {
     }
 
 
-//    @Test
-//    void should_return_twice_car_when_fetch_given_twice_ticket_and_twice_parkingBoy() {
-//
-//        //given
-//        ParkingLot parkingLot=new ParkingLot(2);
-//        ParkingBoy parkingBoy=new ParkingBoy(parkingLot);
-//        Car car1=new Car();
-//        Car car2=new Car();
-//        Ticket ticket1=parkingBoy.park(car1);
-//        Ticket ticket2=parkingBoy.park(car1);
-//        //when
-//        Car myCar=parkingBoy.fetch(ticket);
-//        //then
-//        Assertions.assertNotNull(myCar);
-//        Assertions.assertEquals(car,myCar);
-//    }
+    @Test
+    void should_return_twice_car_when_fetch_given_twice_ticket_and_twice_parkingBoy() {
+
+        //given
+        ParkingLot parkingLot=new ParkingLot(2);
+        ParkingBoy parkingBoy=new ParkingBoy(parkingLot);
+        Car car1=new Car();
+        Car car2=new Car();
+        Ticket ticket1=parkingBoy.park(car1);
+        Ticket ticket2=parkingBoy.park(car2);
+        //when
+        Car myCar1=parkingBoy.fetch(ticket1);
+        Car myCar2=parkingBoy.fetch(ticket2);
+        //then
+        Assertions.assertNotNull(myCar1);
+        Assertions.assertNotNull(myCar2);
+        Assertions.assertEquals(car1,myCar1);
+        Assertions.assertEquals(car2,myCar2);
+    }
 }
